18657ff893d162adbf20b407367be16141abd114
Alex Schmidt <alex.schmidt767@gmail.com>
2022-11-09 23:07:43 -0600

    custom increment can now be done with form

--- src/Counter.js
+++ src/Counter.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { increment, decrement } from "./features/counterSlice";
+import { increment, decrement, incrementByAmount } from "./features/counterSlice";
 
 export default function Counter() {
     // call useSelector to grab the current value of our state variable
@@ -8,6 +8,13 @@ export default function Counter() {
     const count = useSelector((state) => state.value);
     // and include the useDispatch hook...
     const dispatch = useDispatch();
+    // state for custom amount
+    const [ input, setInput ] = useState(0);
+
+    const byAmountSubmit = (e) => {
+        e.preventDefault();
+        dispatch(incrementByAmount(Number(input)));
+    };
 
     return (
         <div>
@@ -16,6 +23,10 @@ export default function Counter() {
             </h1>
             <button onClick={() => dispatch(increment())}>Increment</button>
             <button onClick={() => dispatch(decrement())}>Decrement</button>
+            <form onSubmit={(e) => byAmountSubmit(e)}>
+                <input type='number' onChange={(e) => setInput(e.target.value)} />
+                <input type='submit' />
+            </form>
         </div>
     );
-}
+};
